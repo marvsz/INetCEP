462b588b6d80b5aed26dca3f6464ba150938be51
Johannes Pfannmüller <johannes.pfannmueller@googlemail.com>
2019-09-28 13:36:09 +0200

    Commentary for ConnectedSensorSingleton

--- nfn-scala/src/main/java/SACEPICN/ConnectedSensorsSingleton.java
+++ nfn-scala/src/main/java/SACEPICN/ConnectedSensorsSingleton.java
@@ -16,10 +16,20 @@ public class ConnectedSensorsSingleton {
         return ConnectedSensorsSingleton.instance;
     }
 
+    /**
+     * Adds a Sensor to the sensor List
+     * @param sensorName the name of the sensor
+     * @param sensor the sensor object
+     */
     public void addSensor(String sensorName, Sensor sensor) {
         sensors.put(sensorName, sensor);
     }
 
+    /**
+     * Removes a Sensor from the sensor List identified by its name
+     * This enables us to dynamically remove Sensors from a node.
+     * @param sensorName the name of the sensor
+     */
     public void removeSensor(String sensorName) {
         sensors.remove("sensorName");
     }
