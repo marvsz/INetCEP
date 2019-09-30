689d504f4c8132d5f71a359f89fd87dcd33eb0f7
Johannes Pfannmüller <johannes.pfannmueller@googlemail.com>
2019-09-30 17:03:31 +0200

    added documentation for some parts of ccnl-interest.h

--- ccn-lite/src/ccnl-core/include/ccnl-interest.h
+++ ccn-lite/src/ccnl-core/include/ccnl-interest.h
@@ -33,7 +33,8 @@
 #endif
 
 /**
- * ?
+ * Defines if the forwarder should forward/retransmit a packet, otherwise it
+ * will be handled internally.
  */
 #ifndef CCNL_PIT_COREPROPAGATES
 #define CCNL_PIT_COREPROPAGATES    0x01
@@ -44,7 +45,7 @@
  */
 struct ccnl_pendint_s {
     struct ccnl_pendint_s *next; /**< pointer to the next list element */
-    struct ccnl_face_s *face;    /** */
+    struct ccnl_face_s *face;    /**< pointer to incoming face  */
     uint32_t last_used;          /** */
 };
 
@@ -61,7 +62,7 @@ struct ccnl_interest_s {
     uint32_t lifetime;                  /**< interest lifetime */
 #define CCNL_PIT_COREPROPAGATES    0x01
     uint32_t last_used;                 /**< last time the entry was used */
-    int retries;
+    int retries;                        /**< current number of executed retransmits. */
 #ifdef USE_NFN_REQUESTS
     struct ccnl_interest_s *keepalive; // the keepalive interest dispatched for this interest
     struct ccnl_interest_s *keepalive_origin; // the interest that dispatched this keepalive interest 
@@ -73,13 +74,13 @@ struct ccnl_interest_s {
 };
 
 /**
- * ?
+ * Creates a new interest of type \ref ccnl_interest_s
  *
  * @param[in] ccnl
  * @param[in] from
  * @param[in] pkt
  *
- * @return
+ * @return  Upon success a new interest of type \ref ccnl_interest_s, otherwise NULL
  */
 struct ccnl_interest_s*
 ccnl_interest_new(struct ccnl_relay_s *ccnl, struct ccnl_face_s *from,
