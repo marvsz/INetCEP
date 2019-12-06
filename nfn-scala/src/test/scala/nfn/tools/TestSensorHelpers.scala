c7af40c63e1eb6b1b3fe08b722cda6cdcc030a5e
Johannes Pfannmüller <johannes.pfannmueller@googlemail.com>
2019-12-06 15:36:41 +0100

    Replace Junit Tests wit Scalatest

--- nfn-scala/src/test/scala/nfn/tools/TestSensorHelpers.scala
+++ nfn-scala/src/test/scala/nfn/tools/TestSensorHelpers.scala
@@ -1,20 +1,25 @@
 package nfn.tools
 
-import org.junit.jupiter.api.Assertions.assertTrue
-import org.junit.jupiter.api.Test
+import org.scalatest.{FlatSpec, Matchers}
 
-class TestSensorHelpers {
-  @Test
-  def testPrintSchemaList = {
-    System.out.println(SensorHelpers.printSchemaList)
-    assertTrue(true)
+class TestSensorHelpers extends FlatSpec with Matchers {
+
+  "printSchemaList" should "Print the schema" in {
+    SensorHelpers.printSchemaList should be ("survivors:[date, sequencenumber, gender, age]\nplug:[sequencenumber, date, value, property, plug_id, household_id, house_id]\ngps:[date, identifier, latitude, longitude, altitude, accuracy, distance, speed]\nvictims:[date, sequencenumber, gender, age]\n")
   }
 
-  @Test
-  def testAddNewSensor={
+  "addSensor" should "Add a new Schema to the schema list" in {
     SensorHelpers.addSensor("survivors","/")
-    val expectedList = "survivors:[date, gender, sequencenumber, age]\nplug:[sequencenumber, date, value, property, plug_id, household_id, house_id]\ngps:[date, identifier, latitude, longitude, altitude, accuracy, distance, speed]\nvictims:[date, sequencenumber, gender, age]\n"
-    assertTrue(expectedList==SensorHelpers.printSchemaList)
+    val expectedList = "survivors:[date, sequencenumber, gender, age]\nplug:[sequencenumber, date, value, property, plug_id, household_id, house_id]\ngps:[date, identifier, latitude, longitude, altitude, accuracy, distance, speed]\nvictims:[date, sequencenumber, gender, age]\n"
+    val actualList = SensorHelpers.printSchemaList()
+    expectedList should be (actualList)
+  }
+
+  "removeSensor" should "Remove a Sensor Schema from the Schema list" in {
+    SensorHelpers.removeSensor("victims")
+    val expectedList = "survivors:[date, sequencenumber, gender, age]\nplug:[sequencenumber, date, value, property, plug_id, household_id, house_id]\ngps:[date, identifier, latitude, longitude, altitude, accuracy, distance, speed]\n"
+    val actualList = SensorHelpers.printSchemaList()
+    expectedList should be (actualList)
   }
 
 }
