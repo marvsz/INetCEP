d321dba6ace1d299e58168f1716015f7e3c7fc0e
Johannes Pfannmüller <johannes.pfannmueller@googlemail.com>
2020-01-21 19:10:42 +0100

    destinguish between exact and longest prefix matching

--- ccn-lite/src/ccnl-contentStore/src/ccnl-prefixTree.c
+++ ccn-lite/src/ccnl-contentStore/src/ccnl-prefixTree.c
@@ -3,7 +3,8 @@
 //
 
 #ifndef CCNL_LINUXKERNEL
-#include "../include/ccnl-content-store.h"
+#include "../include/ccnl-contentStore.h"
+#include "../include/ccnl-prefixTree.h"
 #include "../include/ccnl-malloc.h"
 #include "../include/ccnl-prefix.h"
 #include "../include/ccnl-pkt.h"
@@ -20,8 +21,8 @@
 #include "../include/ccnl-defs.h"
 #endif
 
-struct ccnl_tree_node_s* ccnl_tree_node_new(char* nodePrefix, int nodePrefixLength, int depth, ccnl_tree_node* prev, ccnl_tree_node* next){
-    struct ccnl_tree_node_s *tn = (struct ccnl_tree_node_s *) ccnl_calloc(1,sizeof(struct ccnl_tree_node_s));
+struct ccnl_content_store_node_s* ccnl_pfxt_node_new(char* nodePrefix, int nodePrefixLength, int depth, struct ccnl_content_store_node_s* prev, struct ccnl_content_store_node_s* next){
+    struct ccnl_content_store_node_s *tn = (struct ccnl_content_store_node_s *) ccnl_calloc(1,sizeof(struct ccnl_content_store_node_s));
     if(!tn){
         return NULL;
     }
@@ -34,26 +35,8 @@ struct ccnl_tree_node_s* ccnl_tree_node_new(char* nodePrefix, int nodePrefixLeng
     return tn;
 }
 
-struct ccnl_tree_node_s* ccnl_tree_node_find(struct ccnl_tree_node_s* node, struct ccnl_prefix_s *pfx){
-    struct ccnl_tree_node_s *currentNode = node;
-    unsigned char *comp = currentNode->depth < pfx->compcnt ? pfx->comp[currentNode->depth] : 0;
-    int clen = currentNode->depth < pfx->compcnt ? pfx->complen[currentNode->depth] : 32;
-    while(currentNode){
-        if(clen == currentNode->nodePrefixLength && memcmp(comp,node->nodePrefix,node->nodePrefixLength)){
-            if(pfx->compcnt == currentNode->depth){
-                return currentNode;
-            }
-            if(pfx->compcnt > currentNode->depth){
-                ccnl_tree_node_find(currentNode->children,pfx);
-            }
-        }
-        currentNode = currentNode->next;
-    }
-    return currentNode;
-}
-
 int
-ccnl_tree_node_free(struct ccnl_tree_node_s *node){
+ccnl_pfxt_node_free(struct ccnl_content_store_node_s *node){
     if(node){
         if(node->pkt){
             ccnl_pkt_free(node->pkt);
@@ -64,4 +47,65 @@ ccnl_tree_node_free(struct ccnl_tree_node_s *node){
         return 0;
     }
     return -1;
+}
+
+struct ccnl_content_store_node_s* ccnl_pfxt_node_get(struct ccnl_content_store_node_s* node, struct ccnl_prefix_s *pfx, int mode){
+    struct ccnl_content_store_node_s *currentNode = node;
+    unsigned char *comp = currentNode->depth < pfx->compcnt ? pfx->comp[currentNode->depth] : 0;
+    int clen = currentNode->depth < pfx->compcnt ? pfx->complen[currentNode->depth] : 32;
+    if(mode == 0){ // longest matching prefix
+        while(currentNode){
+            if(clen == currentNode->nodePrefixLength && memcmp(comp,node->nodePrefix,node->nodePrefixLength)){
+                if(pfx->compcnt == currentNode->depth){
+                    return currentNode;
+                }
+                if(pfx->compcnt > currentNode->depth){
+                    ccnl_pfxt_node_get(currentNode->children,pfx,mode);
+                }
+            }
+            currentNode = currentNode->next;
+        }
+        return currentNode;
+    }
+    else {
+        while(currentNode){ // exact prefix
+            if(clen == currentNode->nodePrefixLength && memcmp(comp,node->nodePrefix,node->nodePrefixLength)){
+                if(pfx->compcnt == currentNode->depth){
+                    return currentNode;
+                }
+                if(pfx->compcnt > currentNode->depth){
+                    ccnl_pfxt_node_get(currentNode->children,pfx,mode);
+                }
+            }
+            currentNode = currentNode->next;
+        }
+        return NULL;
+    }
+
+}
+
+int ccnl_pfxt_insert_content(struct ccnl_content_store_node_s *node, struct ccnl_pkt_s *pkt){
+
+}
+
+int ccnl_pfxt_contains_content(struct ccnl_content_store_node_s *node, struct ccnl_prefix_s *pfx, int mode){
+    struct ccnl_content_store_node_s *currentNode = node;
+    unsigned char *comp = currentNode->depth < pfx->compcnt ? pfx->comp[currentNode->depth] : 0;
+    int clen = currentNode->depth < pfx->compcnt ? pfx->complen[currentNode->depth] : 32;
+    while(currentNode){
+        if(clen == currentNode->nodePrefixLength && memcmp(comp,node->nodePrefix,node->nodePrefixLength)){
+            if(pfx->compcnt == currentNode->depth){
+                return 1;
+            }
+            if(pfx->compcnt > currentNode->depth){
+                ccnl_pfxt_node_get(currentNode->children,pfx,mode);
+            }
+        }
+        currentNode = currentNode->next;
+    }
+    return 0;
+}
+
+int ccnl_pfxt_remove_content(struct ccnl_content_store_node_s *node, struct ccnl_prefix_s *pfx){
+
 }
