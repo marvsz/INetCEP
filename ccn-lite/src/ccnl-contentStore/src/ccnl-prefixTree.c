557d0e7e02f137c329f6d0460ce13ed31a425929
Johannes Pfannmüller <johannes.pfannmueller@googlemail.com>
2020-01-22 13:07:58 +0100

    Delete Function for Prefixtree

--- ccn-lite/src/ccnl-contentStore/src/ccnl-prefixTree.c
+++ ccn-lite/src/ccnl-contentStore/src/ccnl-prefixTree.c
@@ -60,7 +60,7 @@ struct ccnl_content_store_node_s* ccnl_pfxt_node_get(struct ccnl_content_store_n
                     return currentNode;
                 }
                 if(pfx->compcnt > currentNode->depth){
-                    ccnl_pfxt_node_get(currentNode->children,pfx,mode);
+                    return ccnl_pfxt_node_get(currentNode->children,pfx,mode);
                 }
             }
             currentNode = currentNode->next;
@@ -74,7 +74,7 @@ struct ccnl_content_store_node_s* ccnl_pfxt_node_get(struct ccnl_content_store_n
                     return currentNode;
                 }
                 if(pfx->compcnt > currentNode->depth){
-                    ccnl_pfxt_node_get(currentNode->children,pfx,mode);
+                    return ccnl_pfxt_node_get(currentNode->children,pfx,mode);
                 }
             }
             currentNode = currentNode->next;
@@ -98,7 +98,7 @@ int ccnl_pfxt_contains_content(struct ccnl_content_store_node_s *node, struct cc
                 return 1;
             }
             if(pfx->compcnt > currentNode->depth){
-                ccnl_pfxt_node_get(currentNode->children,pfx,mode);
+                return ccnl_pfxt_contains_content(currentNode->children,pfx,mode);
             }
         }
         currentNode = currentNode->next;
@@ -106,6 +106,75 @@ int ccnl_pfxt_contains_content(struct ccnl_content_store_node_s *node, struct cc
     return 0;
 }
 
-int ccnl_pfxt_remove_content(struct ccnl_content_store_node_s *node, struct ccnl_prefix_s *pfx){
-
+int ccnl_pfxt_remove_content_rec(struct ccnl_content_store_node_s *parent, struct ccnl_content_store_node_s *child, struct ccnl_prefix_s *pfx){
+    struct ccnl_content_store_node_s *currentNode = child;
+    unsigned char *comp = currentNode->depth < pfx->compcnt ? pfx->comp[currentNode->depth] : 0;
+    int clen = currentNode->depth < pfx->compcnt ? pfx->complen[currentNode->depth] : 32;
+    while(currentNode){
+        if(clen == currentNode->nodePrefixLength && memcmp(comp,currentNode->nodePrefix,currentNode->nodePrefixLength)){
+            if(pfx->compcnt == currentNode->depth){ // passenden Knoten gefunden
+                if(!currentNode->children){ // wenn der CurrentNode keine Kinder mehr hat
+                    if (!currentNode->next){ // wenn der zu löschende Knoten keine nächsten Elemente mehr hat --> man kann den Elternknoten löschen
+                        parent->children = NULL;
+                        ccnl_pfxt_node_free(currentNode);
+                        return 1;
+                    }
+                    else{ // wenn der zu löschende Knoten noch nächste Elemente hat
+                        parent->children = currentNode->next;
+                        ccnl_pfxt_node_free(currentNode);
+                        return 0;
+                    }
+                }// Wenn der CurrentNode noch Kinder hat --> nur Paket löschen
+                ccnl_pkt_free(currentNode->pkt);
+                return 0;
+            }
+            if(ccnl_pfxt_remove_content_rec(currentNode,currentNode->children,pfx)){ // keine Kinder mehr da im currentNode
+                if(!currentNode->pkt){ // wenn der currentNode keine Pakete enthält
+                    if(!currentNode->next) { // wenn der current Node kein nächstes hat --> current Node Löschen
+                        parent->children = NULL;
+                        ccnl_pfxt_node_free(currentNode);
+                        return 1;
+                    }
+                    else{ // wenn der currentNode was nächstes hat --> children pointer vom parent umsetzen und currentNode löschen
+                        parent->children = currentNode->next;
+                        ccnl_pfxt_node_free(currentNode);
+                        return 0;
+                    }
+                }
+                return 0; // currentNode enthält noch weitere Pakete
+            }
+            return 0; // es sind noch Kinder im CurrentNode
+        }
+        currentNode = currentNode->next;
+    }
+    return -1; // zu löschendes Element nicht gefunden (sollte nicht auftreten)
 }
+
+int ccnl_pfxt_remove_content(struct ccnl_content_store_node_s *root, struct ccnl_prefix_s *pfx){
+    struct ccnl_content_store_node_s *currentNode = root;
+    int removeStatus = ccnl_pfxt_remove_content_rec(currentNode,currentNode->children,pfx);
+    if(removeStatus == 1){ // keine Kinder mehr da im currentNode
+        if(!currentNode->pkt){ // wenn der currentNode keine Pakete enthält
+            if(!currentNode->next) { // wenn der current Node kein nächstes hat --> current Node Löschen
+                root = NULL;
+                ccnl_pfxt_node_free(currentNode);
+                return 1;
+            }
+            else{ // wenn der currentNode was nächstes hat --> children pointer vom parent umsetzen und currentNode löschen
+                root->children = currentNode->next;
+                ccnl_pfxt_node_free(currentNode);
+                return 0;
+            }
+        }
+        return 0; // currentNode enthält noch weitere Pakete
+    }
+    else{
+        if(removeStatus == 0){
+            return 0; // es sind noch Kinder im CurrentNode
+        } else{
+            return -1; // der zu löschende Knoten wurde nicht gefunden (sollte nicht auftreten)
+        }
+    }
+}
+
+
