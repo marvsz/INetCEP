abb0e04a05642cafe4f22e644d3f3233e580a3bf
Johannes Pfannmüller <johannes.pfannmueller@googlemail.com>
2020-03-05 17:51:59 +0100

    Debugmessage

--- ccn-lite/src/ccnl-core/src/ccnl-relay.c
+++ ccn-lite/src/ccnl-core/src/ccnl-relay.c
@@ -832,6 +832,7 @@ ccnl_do_ageing(void *ptr, void *dummy)
                 // than being held indefinitely, except for Interests that were added with the addQueryInterest package"
         if (!i->isPersistent && ((i->last_used + i->lifetime) <= (uint32_t) t ||
                                 i->retries >= CCNL_MAX_INTEREST_RETRANSMIT)) {
+            DEBUGMSG(TRACE, "The Prefix of the interest was %s\n", ccnl_prefix_to_str(i->pkt->pfx,s,CCNL_MAX_PREFIX_SIZE));
             DEBUGMSG(TRACE, "Interest Lifetime was %i\n",i->lifetime);
             DEBUGMSG(TRACE, "Time at the Moment is %i\n", (uint32_t) t);
             DEBUGMSG(TRACE, "Last Used was %lu\n",i->last_used);
