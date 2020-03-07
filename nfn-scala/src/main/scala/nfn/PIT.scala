c5ce61efb32f63bc8c29291c0413bc8e6c973cce
Johannes Pfannmüller <johannes.pfannmueller@googlemail.com>
2020-03-07 01:14:21 +0100

    Fix the PIT to only delete one actor and not the entire pending interests if there is a persistrent interest from an actor

--- nfn-scala/src/main/scala/nfn/PIT.scala
+++ nfn-scala/src/main/scala/nfn/PIT.scala
@@ -3,6 +3,7 @@ package nfn
 import ccn.packet.CCNName
 import akka.actor.{ActorContext, ActorRef}
 import com.typesafe.scalalogging.LazyLogging
+import scala.collection.mutable.Set
 import scala.collection.mutable
 import scala.concurrent.duration._
 
@@ -19,25 +20,30 @@ case class PIT(context: ActorContext) extends LazyLogging {
 
   private case class Timeout(name: CCNName, face: ActorRef)
 
-  val pit = mutable.Map[CCNName, Set[ActorRef]]()
+  val pit = mutable.Map[CCNName, Set[(ActorRef,Boolean)]]()
 
 
 
-  def add(name: CCNName, face: ActorRef, timeout: FiniteDuration) = {
-    pit += name -> (pit.getOrElse(name, Set()) + face)
+  def add(name: CCNName, isPersistent: Boolean, face: ActorRef, timeout: FiniteDuration) = {
+    pit += name -> (pit.getOrElse(name, Set()).union(Set((face,isPersistent))))
   }
 
-  def get(name: CCNName): Option[Set[ActorRef]] = pit.get(name)
+  def get(name: CCNName): Option[Set[ActorRef]] = pit.get(name).map(_.map(_._1))
 
-  def remove(name: CCNName): Option[Set[ActorRef]] = {
-    pit.remove(name) match {
-      case Some(value) =>
-        logger.debug("Removed from the pit\n")
-        Option.apply(value)
-      case _ =>
-        logger.error(s"Could not remove from the pit. Stacktrace: " + Thread.currentThread().getStackTrace().toString)
-        None
-    }
+  def remove(name: CCNName, pendingFace: ActorRef): Option[Set[ActorRef]] = {
+    pit.get(name).get -= ((pendingFace, false))
+    if (pit.get(name).get.isEmpty)
+      pit.remove(name) match {
+        case Some(value) =>
+          logger.debug("Removed from the pit\n")
+          Option.apply(value).map(_.map(_._1))
+        case _ =>
+          logger.error(s"Could not remove from the pit. Stacktrace: " + Thread.currentThread().getStackTrace().toString)
+          None
+      }
+    else
+    logger.debug("Did not remove since there were still persistent entries")
+      None
   }
 
   override def toString() = {
