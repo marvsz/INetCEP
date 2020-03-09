2ca198def88061f2b102f2796ee3a9a5bbea90e9
Johannes Pfannmüller <johannes.pfannmueller@googlemail.com>
2020-03-09 14:59:51 +0100

    alteration

--- Test scripts/TroughputTest.sh
+++ Test scripts/TroughputTest.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 echo "Bash version ${BASH_VERSION}..."
 var1=$1
-var2=200 #The messages a node sensor is capable to produce at maximum
+var2=160 #The messages a node sensor is capable to produce at maximum. PC:200 remote:50
 var3=$(($var1/$var2))
 if [ "$var3" -eq "0" ]
 then
@@ -9,7 +9,7 @@ screen -d -m ~/INetCEP/ccn-lite/bin/ccn-lite-mkS -n victims -i 1 -t 2 -s $(bc <<
 else
 for ((c=1; c<=$var3; c++))
 do
-	screen -d -m ~/INetCEP/ccn-lite/bin/ccn-lite-mkS -n victims -i $c -t 2 -s 5 -x mgmt-nfn-relay-a.sock -v evaluation
+	screen -d -m ~/INetCEP/ccn-lite/bin/ccn-lite-mkS -n victims -i $c -t 2 -s 20 -x mgmt-nfn-relay-a.sock -v evaluation
 done
 echo $var3
 fi
