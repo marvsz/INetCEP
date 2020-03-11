883f40128be33b1f49f74e09530c983bdd360845
Johannes Pfannmüller <johannes.pfannmueller@googlemail.com>
2020-03-12 00:52:51 +0100

    Some scripts

--- Test scripts/start1node.sh
+++ Test scripts/start1node.sh
@@ -2,8 +2,7 @@
 
 nohup bash startNode.sh 9001 /tmp/mgmt-nfn-relay-a.sock > Logs/node.log &
 sleep 1s
-nohup bash Throughput.sh $1
-nohup bash ConstantPinging.sh $1
+nohup bash ThroughputTest.sh $1
+nohup bash ConstantPingingTest.sh $1
 sleep 1s
 echo "startet everything"
-#~/INetCEP/ccn-lite/bin/ccn-lite-mkS -n victims -i 1 -t 2 -s 100 -x mgmt-nfn-relay-a.sock -v evaluation
