d68b184a9ced5b6579bad6a4638c66e21d46ccac
Johannes Pfannmüller <johannes.pfannmueller@googlemail.com>
2020-03-11 23:12:19 +0100

    Alter startup scripts

--- Test scripts/ConstantPinging.sh
+++ Test scripts/ConstantPinging.sh
@@ -1,16 +1,5 @@
 #!/bin/bash
-Interval=$(bc <<<  "scale = 3; 1 / $1")
-count=0
-while true
-do
-tstart=$(($(date +%s%N)/1000))
-~/INetCEP/ccn-lite/bin/ccn-lite-sendI -s ndn2013 -u 127.0.0.1/9001 "node/nodeA/sensor/victims" &
-tend=$(($(date +%s%N)/1000))
-tDelta=$(($tend-$tstart))
-tDeltaSeconds=$(bc <<<  "scale = 3; $tDelta / 1000000")
-tDeltaDelay=$(bc <<< "scale = 3; $Interval - $tDeltaSeconds")
-(( count++ ))
-echo "Recieved $count Packets so far."
-#echo "Sellp for $tDeltaDelay Seconds."
-#sleep $tDeltaDelay
-done
+Interval=$(bc <<< "scale = 0; 1000 / $1")
+echo $Interval
+screen -d -m bash -c "bash /home/johannes/INetCEP/Test\ scripts/SendI.sh ndn2013 node/nodeA/sensor/victims $Interval trace &> Logs/consumer.log"
+
