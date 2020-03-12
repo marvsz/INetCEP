00c0508952f7ab05ad95dbdf941b1231359d4cb6
Johannes Pfannmüller <johannes.pfannmueller@googlemail.com>
2020-03-12 18:51:24 +0100

    Change scripts

--- Test scripts/start1node.sh
+++ Test scripts/start1node.sh
@@ -6,7 +6,7 @@ sleep 1s
 #nohup ~/INetCEP/ccn-lite/bin/ccn-lite-peekConstant -s ndn2013 -u 127.0.0.1/9001 "node/nodeA/sensor/victims" -v trace &> Logs/$1/$2/ConstantConsumer.log &
 sleep 2s
 nohup bash ThroughputTest.sh $1 $2
-#nohup bash ConstantPingingTest.sh $1 $2
+nohup bash ConstantPingingTest.sh $1 $2
 sleep 1s
 echo "startet everything"
 sleep $3s
