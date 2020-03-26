920d41f1d79ac194742f72c39152b03df51f90f2
Johannes Pfannmüller <johannes.pfannmueller@googlemail.com>
2020-03-26 19:06:08 +0100

    Adjust scripts

--- Test scripts/start1relay.sh
+++ Test scripts/start1relay.sh
@@ -2,6 +2,7 @@
 
 nohup bash startNode.sh 9001 /tmp/mgmt-nfn-relay-a.sock > node.log &
 sleep 2
-nohup bash startCS.sh a 9001 9011 > CS.log &
-sleep 6
+#nohup bash startCS.sh a 9001 9011 > CS.log &
+#sleep 6
+#nohup bash ThroughputTest.sh 500000 1
 #~/INetCEP/ccn-lite/bin/ccn-lite-mkS -n victims -i 1 -t 2 -s 500 -x mgmt-nfn-relay-a.sock -v evaluation
